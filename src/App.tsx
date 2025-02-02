ee9b1839779e8ec744db2668b38495ce607a9134
Suresh Kumar <thakursureshkumar118@gmail.com>
2025-02-02 14:57:55 +0530

    hlo

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import './App.css'
 function App() {
   return (
     <div className='border bg-[#fffdfda1]  border-[#beb8b8] rounded-2xl h-[50vh]  flex items-center justify-center'>
-    This is the main branch . Which contains just project setup and not any functional code .
+    
     </div>
   );
 }
