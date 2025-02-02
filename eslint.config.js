96de371304c6c630318f3bca356a2cf5fa0737d4
Suresh Kumar <thakursureshkumar118@gmail.com>
2025-02-02 13:57:53 +0530

    reinitialized

--- eslint.config.js
+++ eslint.config.js
@@ -1,38 +1,40 @@
 import js from '@eslint/js'
 import globals from 'globals'
-import react from 'eslint-plugin-react'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
+import tseslint from 'typescript-eslint'
 
-export default [
+export default tseslint.config(
   { ignores: ['dist'] },
   {
-    files: ['**/*.{js,jsx}'],
+    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    files: ['**/*.{ts,tsx}'],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
-      parserOptions: {
-        ecmaVersion: 'latest',
-        ecmaFeatures: { jsx: true },
-        sourceType: 'module',
-      },
     },
-    settings: { react: { version: '18.3' } },
     plugins: {
-      react,
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
     },
     rules: {
+<<<<<<< HEAD
       ...js.configs.recommended.rules,
       ...react.configs.recommended.rules,
       ...react.configs['jsx-runtime'].rules,
       ...reactHooks.configs.recommended.rules,
       'react/jsx-no-target-blank': 'off',
+=======
+      ...reactHooks.configs.recommended.rules,
+>>>>>>> e6cf68a (intilizing the main brach)
       'react-refresh/only-export-components': [
         'warn',
         { allowConstantExport: true },
       ],
     },
   },
+<<<<<<< HEAD
 ]
+=======
+)
+>>>>>>> e6cf68a (intilizing the main brach)
---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+
+
+import './App.css'
+
+function App() {
+  return (
+    <div className='border bg-[#fffdfda1]  border-[#beb8b8] rounded-2xl h-[50vh]  flex items-center justify-center'>
+    This is the main branch . Which contains just project setup and not any functional code .
+    </div>
+  );
+}
+export default App
