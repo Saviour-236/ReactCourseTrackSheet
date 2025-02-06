b2c4ff9111f66ffe538d7375f669e0a9ecc6da32
Suresh Kumar <thakursureshkumar118@gmail.com>
2025-02-07 01:29:28 +0530

    workig on tic tac toe

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,38 @@
-import bgImage from "./assets/background.jpg"
 import PlayerTemplate from './ReusableComponents/PlayerTemplate'
 import './App.css'
 import { useState } from "react";
 import GameBoard from "./ReusableComponents/GameBoard";
 
 function App() {
-  const boxes = useState([
+  const [boxes,setBoxex] = useState([
     [null, null, null],
     [null, null, null],
     [null, null, null]
   ])
+  const shiftPlayerTurn = (sign:string,row:any,col:any) => {
+    setBoxex((prevBox:any)=>{
+      let newbox = [...prevBox]
+      console.log(newbox)
+      newbox[row][col] = sign;
+      return newbox
+    })
+   if(sign=="X"){
+      setActivePlayerSign('0')
+      return
+   }
+    setActivePlayerSign('X')
+  }
+  const [activePlayerSign,setActivePlayerSign] = useState('X')
   return (
     <>
-      <main className="h-[80vh] bg-center bg-contain rounded-2xl w-[80vw] m-auto mt-[3rem]" style={{ backgroundImage: `url(${bgImage})` }}>
-        <div className="flex gapx-[-3rem] justify-around ">
-          <PlayerTemplate playerName="suresh" playerSign="0" />
-          <PlayerTemplate playerName="suresh" playerSign="X" />
-        </div>
-        <GameBoard boxes={boxes}/>
+      <main className=" min-w-fit relative bg-center bg-contain rounded-2xl w-[80vw] m-auto mt-[3rem] bg-[#eeeeee30] p-[3rem] " >
+          <div className="flex justify-around ">
+              <PlayerTemplate playerName="suresh" playerSign="0" />
+              <PlayerTemplate playerName="suresh" playerSign="X" />
+          </div>
+          <article className="flex justify-center ">
+                <GameBoard boxes={boxes} shiftPlayerTurn={shiftPlayerTurn} activePlayerSign = {activePlayerSign} />
+          </article> 
       </main>
     </>
   );
--- src/ReusableComponents/GameBoard.tsx
+++ src/ReusableComponents/GameBoard.tsx
@@ -1,27 +1,25 @@
-export default function GameBoard({ boxes }: { boxes: any }) {
-    console.log("boxes", boxes);
+import X from '../assets/letter-x.png'
+import O from '../assets/letter-o.png'
+export default function GameBoard({ boxes ,shiftPlayerTurn ,activePlayerSign,}: { boxes: any,shiftPlayerTurn:any,activePlayerSign :any }) {
+console.log(activePlayerSign)
     return (
-        <>
+        <ul className="border w-fit p-[1rem]  rounded-xl mt-[2.2rem] space-y-[2rem] bg-[#000000]">
             {
-                boxes.map((row: any, rowIndex: any) => { // Renamed index to rowIndex for clarity
-                    return (
-                        <ul key={rowIndex}> {
-                            /* Key is important for React's reconciliation */}
-                            {
-                                row.map((box: any, boxIndex: any) => { 
-                                    // Renamed index to boxIndex
-                                    return (
-                                        <li key={boxIndex} className="border border-[#b8bdc7] rounded-2xl p-[1rem] text-[#b8bdc7] bg-[#1e2446e3] text-2xl">
-                                            {/* hlooooo */} {/* Placeholder text - replace with box content */}
-                                            {box} {/* Display the content of the box */}
-                                        </li>
-                                    )
-                                })
-                            }
-                        </ul>
-                    )
-                })
+                boxes.map((row:any, rowIndex:any) => {
+                   return <li key={rowIndex} className="flex space-x-[2rem] "> {/* Important: Add a key when mapping */}
+                        {row.map((col:any, colIndex:any) => {
+                            console.log(row,col)
+                            return <button 
+                            onClick={()=>shiftPlayerTurn(activePlayerSign,rowIndex,colIndex)}
+                            key={colIndex} 
+                            className="border rounded-2xl min-h-[6rem] min-w-[6rem] cursor-pointer bg-[#96717147] flex justify-center items-center p-[1rem]"> {/* Important: Add a key when mapping */}
+                               {col && <img src={activePlayerSign === "X" ? X : O }  className='h-[5rem]' alt="Plyaer X sign " /> }
+                               {/* */}
+                            </button>
+                        })}
+                    </li>
+})
             }
-        </>
-    )
+        </ul>
+    );
 }
